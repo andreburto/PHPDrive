1d6e4b91a21ee67baf6824d09cc6cec5c70d6492
Andrew Burton <tuglyraisin@gmail.com>
2015-02-26 23:19:06 -0600

    Little more work.

--- PHPDrive.php
+++ PHPDrive.php
@@ -1,25 +1,21 @@
 <?php
 
+define("PHPDRIVE_SQLITE", 1);
+define("PHPDRIVE_SQLITE3", 2);
+define("PHPDRIVE_MYSQLI", 3);
+
 interface IPHPDriveDatabase {
     public function WriteQuery($sql=null);
     public function ReadQuery($sql=null);
-    public function OpenConn($db=null);
     public function CloseConn();
+    public function OpenConn($database=null);
 }
 
 class PHPDriveSqlite implements IPHPDriveDatabase {
     public $CONN;
     public $ERRMSG;
 
-    protected $SELECTALL = "SELECT * FROM fs WHERE FILE = '%s' ORDER BY UPDATED DESC";
-    protected $SELECTONE = "SELECT * FROM fs WHERE FILE = '%s' ORDER BY UPDATED DESC LIMIT 1";
-    protected $CREATE = "CREATE TABLE fs (FILE TEXT,DATA TEXT,CREATED DATETIME,UPDATED DATETIME)";
-    protected $INSERT = "INSERT INTO fs (FILE,DATA,CREATED,UPDATED) VALUES ('%s','%s',%s,%s)";
-    protected $DELETE = "DELETE FROM fs WHERE FILE = '%s'";
-
-    function __construct($database=null) {
-
-    }
+    function __construct() { }
 
     function __destruct() {
         if (isset($this->CONN)) { $this->CloseConn(); }
@@ -52,22 +48,66 @@ class PHPDriveSqlite implements IPHPDriveDatabase {
         return $rows;
     }
 
-    public function OpenConn($db=null) {
-        $this->CONN = sqlite_open($db, 0666);
+    public function CloseConn() {
+        sqlite_close($this->CONN);
+    }
+
+    public function OpenConn($database=null) {
+        $this->CONN = sqlite_open($database, 0666);
+    }
+}
+
+class PHPDriveSqlite3 implements IPHPDriveDatabase {
+    public $CONN;
+    public $ERRMSG;
+
+    function __construct() { }
+
+    function __destruct() {
+        if (isset($this->CONN)) { $this->CloseConn(); }
+    }
+
+    public function WriteQuery($sql=null) {
+        $res = $this->CONN->exec($sql);
+        if ($res == false) {
+            $this->ERRMSG = "Sqlite3 could not write data";
+            return false;
+        }
+        return $res;
+    }
+
+    public function ReadQuery($sql=null) {
+        $rows = array();
+        $res = $this->CONN->query($sql);
+        if (is_object($res) == false) {
+            $this->ERRMSG = "Sqlite3 found no results";
+            return false;
+        }
+        while($row = $res->fetchArray()) {
+            $rows[] = $row;
+        }
+        if (count($rows) == 0) {
+            $this->ERRMSG = "Sqlite3 looped no results";
+            return false;
+        }
+        return $rows;
     }
 
     public function CloseConn() {
-        sqlite_close($this->CONN);
+        $this->CONN->close();
+    }
+
+    public function OpenConn($database=null) {
+        $this->CONN = new Sqlite3($this->FILE, SQLITE3_OPEN_READWRITE | SQLITE3_OPEN_CREATE);
     }
 }
 
 class PHPDrive {
     // Class-wide variables
-    public $TYPE;
     public $FILE;
-    public $CONN;
     public $ERRMSG;
-    
+    protected $DBOBJ;
+
     // The SQL used below
     protected $SELECTALL = "SELECT * FROM fs WHERE FILE = '%s' ORDER BY UPDATED DESC";
     protected $SELECTONE = "SELECT * FROM fs WHERE FILE = '%s' ORDER BY UPDATED DESC LIMIT 1";
@@ -87,107 +127,48 @@ class PHPDrive {
         }
         // Set DB type
         if ($dbtype == null) {
-            $this->TYPE = $this->SelectDatabaseType();
+            $this->DBOBJ = $this->SelectDatabaseType();
         } else {
-            $this->TYPE = $dbtype;
+            switch($dbtype) {
+                case PHPDRIVE_SQLITE:
+                    $this->DBOBJ = new PHPDriveSqlite();
+                    break;
+                case PHPDRIVE_SQLITE3:
+                    $this->DBOBJ = new PHPDriveSqlite3();
+                    break;
+            }
         }
         // Check for the file
         if (is_file($this->FILE)) {
             // Connect to the database
-            $this->OpenConn($this->FILE);
+            $this->DBOBJ->OpenConn($this->FILE);
         } else {
             // Connect to the database
-            $this->OpenConn($this->FILE);
+            $this->DBOBJ->OpenConn($this->FILE);
             // Create the table
-            $this->WriteQuery($this->CREATE);
+            $this->DBOBJ->WriteQuery($this->CREATE);
         }
     }
     
     function __destruct() {
-        if (isset($this->CONN)) { $this->CloseConn(); }
+        if (isset($this->DBOBJ)) { $this->DBOBJ->CloseConn(); }
+        $this->DBOBJ = null;
     }
     
     protected function SelectDatabaseType() {
         if (function_exists("sqlite_open")) {
-            return "sqlite";
+            return new PHPDriveSqlite();
         } else {
-            return "Sqlite3";
+            return new PHPDriveSqlite3();
         }
     }
-    
-    protected function WriteQuery($sql=null) {
-        if ($this->TYPE == "sqlite") {
-            $res = sqlite_exec($this->CONN, $sql);
-            if ($res == false) {
-                $this->ERRMSG = "Sqlite2 could not write data";
-                return false;
-            }
-        } else {
-            $res = $this->CONN->exec($sql);
-            if ($res == false) {
-                $this->ERRMSG = "Sqlite3 could not write data";
-                return false;
-            }
-        }
-        return $res;
-    }
-    
-    protected function ReadQuery($sql=null) {
-        $rows = array();
-        if ($this->TYPE == "sqlite") {
-            $res = sqlite_query($this->CONN, $sql, SQLITE_ASSOC, $this->ERRMSG);
-            if ($this->ERRMSG) { return false; }
-            if ($res == false) {
-                $this->ERRMSG = "Sqlite2 query failed";
-                return false;
-            }
-            if (sqlite_num_rows($res) === 0) {
-                $this->ERRMSG = "Sqlite2 returned no results";
-                return false;
-            }
-            while($row = sqlite_fetch_array($res, SQLITE_ASSOC)) {
-                $rows[] = $row;
-            }
-        } else {
-            $res = $this->CONN->query($sql);
-            if (is_object($res) == false) {
-                $this->ERRMSG = "Sqlite3 found no results";
-                return false;
-            }
-            while($row = $res->fetchArray()) {
-                $rows[] = $row;
-            }
-            if (count($rows) == 0) {
-                $this->ERRMSG = "Sqlite3 looped no results";
-                return false;
-            }
-        }
-        return $rows;
-    }
-    
-    protected function OpenConn() {
-        if ($this->TYPE == "sqlite") {
-            $this->CONN = sqlite_open($this->FILE, 0666);
-        } else {
-            $this->CONN = new Sqlite3($this->FILE, SQLITE3_OPEN_READWRITE | SQLITE3_OPEN_CREATE);
-        }
-    }
-    
-    protected function CloseConn() {
-        if ($this->TYPE == "sqlite") {
-            sqlite_close($this->CONN);
-        } else {
-            $this->CONN->close();
-        }
-        $this->CONN = null;
-    }
-    
+
     public function GetFile($name=null) {
         if ($name == null) {
             $this->ERRMSG = "No file name provided";
             return false;
         }
-        $rows = $this->ReadQuery(sprintf($this->SELECTALL, $name));
+        $rows = $this->DBOBJ->ReadQuery(sprintf($this->SELECTALL, $name));
         return $rows;
     }
     
@@ -196,7 +177,7 @@ class PHPDrive {
             $this->ERRMSG = "No file name provided";
             return false;
         }
-        $rows = $this->ReadQuery(sprintf($this->SELECTONE, $name));
+        $rows = $this->DBOBJ->ReadQuery(sprintf($this->SELECTONE, $name));
         return $rows[0];
     }
     
@@ -206,7 +187,7 @@ class PHPDrive {
             return false;
         }
         if ($data == null) { $data = ""; }
-        $res = $this->WriteQuery(sprintf($this->INSERT,$name,$data,time(),time()));
+        $res = $this->DBOBJ->WriteQuery(sprintf($this->INSERT,$name,$data,time(),time()));
         if ($res == false) {
             $this->ERRMSG = "Could not insert data";
             return false;
@@ -216,7 +197,7 @@ class PHPDrive {
     
     public function UpdateFile($name=null, $data=null) {
         if ($name == null) {
-            $this->ERRMSG = "Missing file name to create a file";
+            $this->DBOBJ->ERRMSG = "Missing file name to create a file";
             return false;
         }
         if ($data == null) { $data = ""; }
@@ -224,7 +205,7 @@ class PHPDrive {
         if (is_array($file) == false) {
             return $this->CreateFile($name, $data);
         }
-        $res = $this->WriteQuery(sprintf($this->INSERT,$name,$data,$file['CREATED'],time()));
+        $res = $this->DBOBJ->WriteQuery(sprintf($this->INSERT,$name,$data,$file['CREATED'],time()));
         if ($res == false) {
             $this->ERRMSG = "Could not insert data";
             return false;
@@ -237,7 +218,7 @@ class PHPDrive {
             $this->ERRMSG = "Missing file name to delete a file";
             return false;
         }
-        $res = $this->WriteQuery(sprintf($this->DELETE, $name));
+        $res = $this->DBOBJ->WriteQuery(sprintf($this->DELETE, $name));
         if ($res == false) {
             $this->ERRMSG = "Could not delete file";
             return false;
